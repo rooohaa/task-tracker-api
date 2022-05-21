eacba3db31fcbc49b688d622c1e872f4aa5b6e29
Rakhat Alpysbai <rakhatdev@gmail.com>
2022-05-21 11:41:12 +0600

    setup gin for routing

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Task Tracker")
+
+	router := gin.Default()
+	
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Task Tracker",
+		})
+	})
+
+	router.Run()
 }
